9f4842d54dc9d31447d193c3d11d397fa5f009c6
Hasibul Hasan <hhasibul677@gmail.com>
2021-10-04 18:00:35 +0600

    icon link added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,18 +12,26 @@ const Footer = () => {
           © 2021 cipywright @Hasibul Hasan
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <NavLink to='' className="text-gray-400">
-          <i className="fa-2x fab fa-facebook"></i>
-          </NavLink>
-          <NavLink to='' className="ml-3 text-gray-400">
-          <i className="fa-2x fab fa-twitter"></i>
-          </NavLink>
-          <NavLink to='' className="ml-3 text-gray-400">
-          <i className="fa-2x fab fa-instagram-square"></i>
-          </NavLink>
-          <NavLink to='' className="ml-3 text-gray-400">
-          <i className="fa-2x fab fa-linkedin-in"></i>
-          </NavLink>
+        <a
+                href="https://facebook.com/"
+                target="blank"
+                className="fa-2x fab fa-facebook mr-5"
+              ></a>
+              <a
+                href="https://twitter.com/"
+                target="blank"
+                className="fa-2x fab fa-twitter mr-5"
+              ></a>
+              <a
+                href="https://www.instagram.com/"
+                target="blank"
+                className="fa-2x fab fa-instagram-square mr-5"
+              ></a>
+              <a
+                href="https://www.linkedin.com/"
+                target="blank"
+                className="fa-2x fab fa-linkedin-in"
+              ></a>
         </span>
       </div>
     </footer>
